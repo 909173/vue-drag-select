6e34afb74c4894cad6c5f6fef863c077ff885be1
909173 <ts.ksc.wss@gmail.com>
2018-09-06 22:56:19 +0900

    make vue files

---
+++ src/components/test.vue
@@ -0,0 +1,43 @@
+<template>
+  <draggable-area
+  >
+    <template>
+      
+    </template>
+  </draggable-area>
+</template>
+
+
+<script lang="ts">
+import Vue from 'vue'
+import draggableAreaVue from '@/draggable-area/draggableArea.vue';
+export default Vue.extend({
+  name: "test",
+  components:{
+    "daggable-area": draggableAreaVue
+  },
+  data() {
+    return {
+      testJson: [
+        {
+          id: 0,
+          name: "jhon"
+        },
+        {
+          id: 1,
+          name: "json"
+        },
+        {
+          id: 2,
+          name: "Kres"
+        }
+      ]
+    }
+  }  
+})
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ src/draggable-area/pulledByMouse.vue
@@ -0,0 +1,14 @@
+<template>
+  
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: "pulledByMouse"
+})
+</script>
+
+
+<style scoped>
+</style>
